1a89fbe545e9ea1efa6d48f62e838484aff67022
Jung Gyu Yoon <yjg30737@gmail.com>
2024-05-02 17:15:58 +0900

    Connect with chat table of Supabase and save chat history

--- main.py
+++ main.py
@@ -13,6 +13,9 @@ st.set_page_config(
 # Initialize connection.
 conn = st.connection('supabase', type=SupabaseConnection)
 
+# Login to Supabase.
+# conn.auth.sign_in_with_password(dict(email=st.secrets['SUPABASE_USER_EMAIL'], password='this_password_is_insecure_and_should_be_updated'))
+
 # Perform query.
 rows = conn.query('*', table='mytable', ttl='10m').execute()
 
@@ -34,9 +37,14 @@ client = OpenAI(api_key=st.secrets['OPENAI_API_KEY'])
 if 'openai_model' not in st.session_state:
     st.session_state['openai_model'] = 'gpt-3.5-turbo'
 
-# Initialize chat history
-if 'messages' not in st.session_state:
-    st.session_state.messages = [{'role': 'system', 'content': 'You are a helpful assistant.'}]
+# Get the chat history from the db
+chat_history = conn.table('chat').select('*').execute()
+if len(chat_history.data) > 0:
+    st.session_state.messages = chat_history.data
+else:
+    # Initialize chat history
+    if 'messages' not in st.session_state:
+        st.session_state.messages = [{'role': 'system', 'content': 'You are a helpful assistant.'}]
 
 # Display chat messages from history on app rerun
 for message in st.session_state.messages:
@@ -48,8 +56,11 @@ if prompt := st.chat_input('What is up?'):
     # Display user message in chat message container
     with st.chat_message('user'):
         st.markdown(prompt)
+    response_obj = {'role': 'user', 'content': prompt}
     # Add user message to chat history
-    st.session_state.messages.append({'role': 'user', 'content': prompt})
+    st.session_state.messages.append(response_obj)
+    # Save to the db
+    conn.table('chat').insert(response_obj).execute()
 
     # Chatbot's response
     # Display assistant repsonse in chat message container
@@ -64,5 +75,8 @@ if prompt := st.chat_input('What is up?'):
             stream=True
         )
         response = st.write_stream(stream)
+    response_obj = {'role': 'assistant', 'content': response}
     # Add assistant response to chat history
-    st.session_state.messages.append({'role': 'assistant', 'content': response})
+    st.session_state.messages.append(response_obj)
+    # Save to the db
+    conn.table('chat').insert(response_obj).execute()
